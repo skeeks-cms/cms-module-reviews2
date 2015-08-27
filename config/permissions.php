880651fb5fc981c0e971e1a01c8a982cfe7f8c31
Semenov <semenov@skeeks.com>
2015-08-27 14:37:33 +0300

    Управление доступом к добавлению отзывов

---
+++ config/permissions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 20.03.2015
+ */
+return [
+
+    'permissions'   => [
+        [
+            'name' => \skeeks\cms\reviews2\components\Reviews2Component::PERMISSION_ADD_REVIEW,
+            'description'   => 'Добавление отзывов'
+        ],
+    ]
+];

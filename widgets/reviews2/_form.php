1fa1e4b55e8525ff6533962b005d83e2eb6fd2eb
Ekilei <ekilei@gmail.com>
2016-05-04 10:33:45 +0300

    4

--- widgets/reviews2/_form.php
+++ widgets/reviews2/_form.php
@@ -8,11 +8,11 @@
 /* @var $this yii\web\View */
 ?>
 
-<?= $form->fieldSet('Отображение'); ?>
+<?= $form->fieldSet(\Yii::t('skeeks/reviews2','Display')); ?>
     <?= $form->field($model, 'viewFile')->textInput(); ?>
 <?= $form->fieldSetEnd(); ?>
 
-<?= $form->fieldSet('Постраничная навигация'); ?>
+<?= $form->fieldSet(\Yii::t('skeeks/reviews2','Pagination')); ?>
     <?= $form->fieldRadioListBoolean($model, 'enabledPaging', \Yii::$app->cms->booleanFormat()); ?>
     <?= $form->fieldRadioListBoolean($model, 'enabledPjaxPagination', \Yii::$app->cms->booleanFormat()); ?>
     <?= $form->fieldInputInt($model, 'pageSize'); ?>
@@ -20,16 +20,16 @@
 
 <?= $form->fieldSetEnd(); ?>
 
-<?= $form->fieldSet('Сортировка и количество'); ?>
+<?= $form->fieldSet(\Yii::t('skeeks/reviews2','Sort and count')); ?>
     <?= $form->fieldInputInt($model, 'limit'); ?>
     <?= $form->fieldSelect($model, 'orderBy', (new \skeeks\cms\reviews2\models\Reviews2Message())->attributeLabels()); ?>
     <?= $form->fieldSelect($model, 'order', [
-        SORT_ASC    => "ASC (от меньшего к большему)",
-        SORT_DESC   => "DESC (от большего к меньшему)",
+        SORT_ASC    => "ASC (".\Yii::t('skeeks/reviews2','from smallest to largest').")",
+        SORT_DESC   => "DESC (".\Yii::t('skeeks/reviews2','from highest to lowest').")",
     ]); ?>
 <?= $form->fieldSetEnd(); ?>
 
-<?= $form->fieldSet('Настройки'); ?>
+<?= $form->fieldSet(\Yii::t('skeeks/reviews2','Settings')); ?>
 
     <?= $form->field($model, 'btnSubmit')->textInput(); ?>
     <?= $form->field($model, 'btnSubmitClass')->textInput(); ?>

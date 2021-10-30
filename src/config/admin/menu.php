82e53885bb7ffd606a204e6c5db72e5d206467d2
Semenov <semenov@skeeks.com>
2021-10-30 12:05:34 +0300

    dev

--- src/config/admin/menu.php
+++ src/config/admin/menu.php
@@ -23,14 +23,14 @@ return [
                                     "img" => ['\skeeks\cms\reviews2\assets\Reviews2Asset', 'icons/reviews.jpg'],
                                 ],
 
-                                [
+                                /*[
                                     "label" => \Yii::t('skeeks/reviews2', "Settings"),
                                     "url" => ["cms/admin-settings", "component" => 'skeeks\cms\reviews2\components\Reviews2Component'],
                                     "img" => ['skeeks\cms\assets\CmsAsset', 'images/icons/settings-big.png'],
                                     "activeCallback" => function ($adminMenuItem) {
                                         return (bool)(\Yii::$app->request->getUrl() == $adminMenuItem->getUrl());
                                     },
-                                ],
+                                ],*/
 
                             ]
                     ],
